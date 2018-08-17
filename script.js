4d9c7570fc332a7511bbb6fb2db48622afc4d02b
Adam Kulpa <ad200@gazeta.pl>
2018-08-17 23:32:53 +0100

    Change html & js

--- script.js
+++ script.js
@@ -2,31 +2,34 @@ var movies = [
   {
     id: 1,
     title: 'Harry Potter',
-    desc: 'film o czarodzieju',
-	pict: src= "http://imgur.com/n8OYCzR.png"
+    desc: 'Film o czarodzieju',
+	pict: "https://moustique.cdnartwhere.eu/sites/default/files/field/image/harry_potter.jpg"
   },
   {
     id: 2,
     title: 'Król Lew',
-    desc: 'Film o królu sawanny'
+    desc: 'Film o królu sawanny',
+	pict: "https://vignette.wikia.nocookie.net/fanowska-krol-lew/images/5/55/Talia.png/revision/latest?cb=20170809093905&path-prefix=pl"
   },
   {
     id: 3,
     title: 'Gra tajemnic',
-    desc: 'Film biograficzny'
+    desc: 'Film biograficzny',
+	pict: "https://m.media-amazon.com/images/M/MV5BOTgwMzFiMWYtZDhlNS00ODNkLWJiODAtZDVhNzgyNzJhYjQ4L2ltYWdlXkEyXkFqcGdeQXVyNzEzOTYxNTQ@._V1_UX182_CR0,0,182,268_AL_.jpg"
   },
   {
     id: 4,
     title: 'Bogowie',
-    desc: 'Film biograficzny'
+    desc: 'Film biograficzny',
+	pict: "https://1.fwcdn.pl/po/43/78/694378/7632114.3.jpg"
   }
 ];
 
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
-      React.createElement('p', {}, movie.desc),
-	  React.createElement('image', {}, movie.pict)
+      React.createElement('p', {alt:"film"}, movie.desc),
+	  React.createElement('img', {src: movie.pict})
     );
 });
 
